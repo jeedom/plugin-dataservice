f97930e29648564d34289ab54d6ee3a76dedd693
Loïc <loic@jeedom.com>
2020-05-10 10:01:40 +0200

    order by name

--- desktop/php/dataservice.php
+++ desktop/php/dataservice.php
@@ -6,6 +6,9 @@ $plugin = plugin::byId('dataservice');
 sendVarToJS('eqType', $plugin->getId());
 $eqLogics = eqLogic::byType($plugin->getId());
 $services = dataservice::devicesParameters();
+uasort($services, function ($a, $b) {
+	return $a['name'] - $b['name'];
+});
 sendVarToJS('dataservice_services',$services);
 ?>
 
